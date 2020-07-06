635db4db02a4d1577a344aed388353fc8d99073a
ring-design <67853336+ring-design@users.noreply.github.com>
2020-07-06 17:58:39 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+$(function () {
+
+  // フロートヘッダーメニュー
+  var targetHeight = $('.js-float-menu-target').height();
+  $(window).on('scroll', function() {
+    $('.js-float-menu').toggleClass('float-active', $(this).scrollTop() > targetHeight);
+  });
+
+  // SPメニュー
+  $('.js-toggle-sp-menu').on('click', function () {
+    $(this).toggleClass('active');
+    $('.js-toggle-sp-menu-target').toggleClass('active');
+  });
+});
+  //メニューをクリックしたらハンバーガーメニューを閉じる!!!!!!
+  $('.menu-link').on('click', function () {
+    $('.js-toggle-sp-menu').toggleClass('active');
+    $('.js-toggle-sp-menu-target').toggleClass('active');
+  });
+    
+
+//トップへ戻るボタン
+$(function(){
+  var pagetop = $('#page_top');
+  // ボタン非表示
+  pagetop.hide();
+  // 100px スクロールしたらボタン表示
+  $(window).scroll(function () {
+     if ($(this).scrollTop() > 100) {
+          pagetop.fadeIn();
+     } else {
+          pagetop.fadeOut();
+     }
+  });
+  pagetop.click(function () {
+     $('body, html').animate({ scrollTop: 0 }, 500);
+     return false;
+  });
+});
+
+  //要素をフワッと表示させる
+  
